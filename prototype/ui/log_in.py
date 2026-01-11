1403e9be5d41a691cc5a5a6c52baf836a084d735
MarcRGM <marcrodniemanalo@gmail.com>
2026-01-11 20:10:06 +0800

    Add inputs

--- prototype/ui/log_in.py
+++ prototype/ui/log_in.py
@@ -1,5 +1,10 @@
 import solara
 
+
+user = solara.reactive("")
+passwr = solara.reactive("")
+continuous_update = solara.reactive(True)
+
 @solara.component
 def Page():
     solara.Style("""
@@ -16,6 +21,7 @@ def Page():
             "height": "100vh",
             "width": "100vw",
             "display": "flex",
+            "flexDirection": "column",
             "justifyContent": "center",
             "alignItems": "center",
             "backgroundColor": "#FADA7A",
@@ -25,3 +31,16 @@ def Page():
         <span style="color:#1C6EA4; font-size:72px; font-weight:bold; font-family: 'Arial', 'Helvetica', sans-serif;">Enti</span>
         <span style="color:#578FCA; font-size:72px; font-weight:bold; font-family: 'Arial', 'Helvetica', sans-serif;">Lytics</span>
         """)
+        solara.InputText(
+            label="Username",
+            value=user,
+            continuous_update=continuous_update.value,
+        )
+        solara.InputText(
+            label="Password",
+            value=passwr,
+            continuous_update=continuous_update.value,
+        )
+
+        with solara.Row():
+            solara.Button("Clear", on_click=lambda: (user.set(""), passwr.set("")))
